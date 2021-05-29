a65998aeed579eecfb4c849da1364c17d3d9dab3
Anton <railanmoon@gmail.com>
2021-05-29 22:33:49 +0300

    added firebase

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -16,66 +16,65 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     var user = context.read<UserModel>();
 
-    return SafeArea(
-      child: Material(
-        child: Container(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 5,
-                child: Image(
-                  image: AssetImage('assets/images/gates_1.png'),
-                ),
+    return Material(
+      child: Container(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Image(
+                image: AssetImage('assets/images/gates_1.png'),
               ),
-              Expanded(
-                flex: 1,
-                child: Text(
-                  "Welcome to Avalon",
-                  style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w700,
-                      fontFamily: 'CinzelDecorative'),
-                ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Text(
+                "Welcome to Avalon",
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'CinzelDecorative'),
               ),
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                    onSubmitted: (String text) {
-                      setState(() {
-                        if (text.length < 4) {
-                          _errorText = "Name must be longer then 3 characters.";
-                          _changeButtonState(true);
-                        } else {
-                          user.username = text;
-                          _changeButtonState(false);
-                          _errorText = null;
-                        }
-                      });
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Enter your name",
-                      errorText: _getErrorText(),
-                    ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: TextField(
+                  onSubmitted: (String text) {
+                    setState(() {
+                      if (text.length < 4) {
+                        _errorText = "Name must be longer then 3 characters.";
+                        _changeButtonState(true);
+                      } else {
+                        user.username = text;
+                        _changeButtonState(false);
+                        _errorText = null;
+                      }
+                    });
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Enter your name",
+                    errorText: _getErrorText(),
                   ),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: ElevatedButton(
-                    onPressed: _isDisabled ? null : _navigate,
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.teal, minimumSize: Size(250.0, 50.0)),
-                    child: Text("Enter Avalon"),
-                  ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 10.0, right: 10.0, bottom: 25.0),
+                child: ElevatedButton(
+                  onPressed: _isDisabled ? null : _navigate,
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.teal, minimumSize: Size(250.0, 50.0)),
+                  child: Text("Enter Avalon"),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
