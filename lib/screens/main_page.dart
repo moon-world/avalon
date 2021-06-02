8d40fcd1899404781d6c9ffc95bb99e8c1092fca
Anton <railanmoon@gmail.com>
2021-06-02 23:41:22 +0300

    Separated EnterLobby to another widget

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:avalon/models/application_state.dart';
 import 'package:avalon/models/user_model.dart';
+import 'package:avalon/services/rt_database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,7 +14,6 @@ class _MainPage extends State<MainPage> {
   Widget build(BuildContext context) {
     var appState = context.watch<ApplicationState>();
     var username = appState.userName;
-    final TextEditingController _textEditController = TextEditingController();
 
     return SafeArea(
       child: Material(
@@ -45,54 +45,11 @@ class _MainPage extends State<MainPage> {
                     ),
                     child: Text("Create Lobby"),
                     onPressed: () {
-                      Navigator.pushNamed(context, '/lobby');
+                      createLobby();
                     },
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.cyan.shade400,
-                          minimumSize: Size(250.0, 50.0)),
-                      child: Text("Enter Lobby"),
-                      onPressed: () => {
-                            showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) => AlertDialog(
-                                      title: const Text('Enter Room Pass'),
-                                      content: const Text('Enter numbers'),
-                                      actions: <Widget>[
-                                        TextFormField(
-                                          controller: _textEditController,
-                                          validator: (value) {
-                                            return value!.isEmpty
-                                                ? null
-                                                : "Invalid";
-                                          },
-                                          decoration: InputDecoration(
-                                              hintText: "Enter Room Number"),
-                                        ),
-                                        TextButton(
-                                          onPressed: () =>
-                                              Navigator.pop(context, 'Cancel'),
-                                          child: Align(
-                                            alignment: Alignment.bottomRight,
-                                            child: Text('Cancel'),
-                                          ), // Need to change this~
-                                        ),
-                                        TextButton(
-                                          onPressed: () => Navigator.pop(
-                                              context, 'Enter Room'),
-                                          child: Align(
-                                            alignment: Alignment.bottomLeft,
-                                            child: Text('Enter Room'),
-                                          ),
-                                        ),
-                                      ],
-                                    ))
-                          }),
-                ),
+                EnterLobbyPopup(),
                 Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: ElevatedButton(
@@ -123,7 +80,71 @@ class _MainPage extends State<MainPage> {
     );
   }
 
+  createLobby() {
+    try {
+      var rdb = new RealTimeDataBase();
+      UserModel user = new UserModel();
+      rdb.CreateLobby(user);
+    } catch (e) {
+      print(e);
+    }
+
+    Navigator.pushNamed(context, '/lobby');
+  }
+
   enterLobby() {}
   navigateToSettings() {}
   exit() {}
+}
+
+class EnterLobbyPopup extends StatefulWidget {
+  @override
+  _EnterLobbyPopup createState() => _EnterLobbyPopup();
+}
+
+class _EnterLobbyPopup extends State<EnterLobbyPopup> {
+  final TextEditingController _textEditController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.cyan.shade400, minimumSize: Size(250.0, 50.0)),
+          child: Text("Enter Lobby"),
+          onPressed: () => {
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Enter Room Pass'),
+                    content: const Text('Enter numbers'),
+                    actions: <Widget>[
+                      TextFormField(
+                        controller: _textEditController,
+                        validator: (value) {
+                          return value!.isEmpty ? null : "Invalid";
+                        },
+                        decoration:
+                            InputDecoration(hintText: "Enter Room Number"),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Text('Cancel'),
+                        ), // Need to change this~
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Enter Room'),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Text('Enter Room'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              }),
+    );
+  }
 }
